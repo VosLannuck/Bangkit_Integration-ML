f8a1004f4341d1a0a30fd0f935a3727313902206
vos <voslannack@gmail.com>
2023-11-23 12:04:17 +0700

    INIT ML

---
+++ Main.py
@@ -0,0 +1,11 @@
+#%%
+import pandas as pd
+import numpy as np
+
+from torch.nn import Module
+from torch.utils.data import Dataset, DataLoadera
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+#%%
